3ad1a730a43cb66861ea5c9e4d46c5710fe2317e
RyanMKrol <ryankrol.m@gmail.com>
2021-03-06 23:04:53 +0000

    Add data format questions

--- src/modules/questions/index.js
+++ src/modules/questions/index.js
@@ -19,6 +19,19 @@ const QUESTIONS = [
     message: 'What is your secret access key?',
     validate: validateRequiredField,
   },
+  {
+    name: 'region',
+    type: 'input',
+    message: 'Which region does your Dynamo table belong to?',
+    validate: validateRequiredField,
+  },
+  {
+    name: 'format',
+    type: 'list',
+    message: 'What format would you like to see the data in?',
+    validate: validateRequiredField,
+    choices: ['json', 'csv'],
+  },
 ];
 
 /**
@@ -36,12 +49,31 @@ function validateRequiredField(input) {
 }
 
 /**
+ * Method to format the raw data from the customer to an object that makes more sense to our API
+ *
+ * @param {object} answers Object representing the answers to the above questions
+ * @returns {object} Formatted answers to fit the calls we'll make later
+ */
+function formatAnswers(answers) {
+  const formattedAnswers = answers;
+  formattedAnswers.credentials = {
+    accessKeyId: answers.credential_access_key,
+    secretAccessKey: answers.credential_secret_access_key,
+  };
+
+  delete formattedAnswers.credential_access_key;
+  delete formattedAnswers.credential_secret_access_key;
+
+  return formattedAnswers;
+}
+
+/**
  * Fetches the answers to the questions above
  *
  * @returns {Promise<Array<string>>} The data needed to dump a dynamo table
  */
 async function prompt() {
-  return inquirer.prompt(QUESTIONS);
+  return inquirer.prompt(QUESTIONS).then((answers) => formatAnswers(answers));
 }
 
 export default prompt;
